d1da193a2252567712b412ffef59b41e6f25eb72
akhilesh-bulbule <akhilesh.bulbule@gmail.com>
2023-03-22 00:41:25 +0530

    added test master in reactive form for create and edit test master

--- angular/src/app/main/library/tests/test-master/create-edit-test-master/create-edit-test-master-modal.component.ts
+++ angular/src/app/main/library/tests/test-master/create-edit-test-master/create-edit-test-master-modal.component.ts
@@ -1,7 +1,7 @@
 import { Component, Injector, ViewChild, ViewEncapsulation } from "@angular/core";
-import { FormBuilder } from "@angular/forms";
+import { FormBuilder, FormControl, FormGroup } from "@angular/forms";
 import { AppComponentBase } from "@shared/common/app-component-base";
-import { TestMasterServiceProxy } from "@shared/service-proxies/service-proxies";
+import { ApplicationsDto, TestMasterDto, TestMasterServiceProxy } from "@shared/service-proxies/service-proxies";
 import { ModalDirective } from 'ngx-bootstrap/modal';
 
 @Component({
@@ -16,6 +16,11 @@ export class CreateOrEditTestModalComponent extends AppComponentBase {
     active = false;
     saving = false;
 
+    testMasterForm!: FormGroup;
+
+
+    applicationList : ApplicationsDto[] = [];
+
     constructor(
         injector: Injector,
         private _testMasterService: TestMasterServiceProxy,
@@ -25,19 +30,56 @@ export class CreateOrEditTestModalComponent extends AppComponentBase {
     }
 
 
-    show(techniqueId?: string): void {
-        if (!techniqueId) {
-           // this.techniqueItem = new TechniqueDto({id : null, name: "", description: ""}); 
+    show(testMasterId?: string): void {
+        if (!testMasterId) {
+            let testMasterItem : TestMasterDto = new TestMasterDto();
+            this.initialiseTestMasterForm(testMasterItem);
             this.active = true;
             this.modal.show();
         }
         else{
-            // this._techniqueService.getTechniqueById(techniqueId).subscribe((response : TechniqueDto)=> {
-            //    this.techniqueItem = response;
-            //     this.active = true;
-            //     this.modal.show();
-            // });
+            this._testMasterService.getTestMasterById(testMasterId).subscribe((response : TestMasterDto)=> {
+                let testMasterItem = response;
+                this.initialiseTestMasterForm(testMasterItem);
+                this.active = true;
+                this.modal.show();
+            });
         }        
+    }
+
+    initialiseTestMasterForm(testItem : TestMasterDto){
+        this.testMasterForm = this.formBuilder.group({
+            name: new FormControl(testItem.name, [
+                // Validators.required,
+            ]),
+            techniqueId: new FormControl(testItem.techniqueId, []),
+            isDefaultTechnique: new FormControl(testItem.isDefaultTechnique, []),
+            applicationId: new FormControl(testItem.applicationId, []),
+            method: new FormControl(testItem.method, []),
+            methodDescription: new FormControl(testItem.methodDescription, []),
+            isSC: new FormControl(testItem.isSC, []),
+            rate: new FormControl(testItem.rate, []),
+            id: new FormControl(testItem.id, []),
+            // customerAddresses: customerItem.id ? this.formBuilder.array(
+            //     customerItem.customerAddresses.map((x : CustomerAddressDto) => 
+            //         this.createCustomerAddress(x)
+            //       )
+            // ) : this.formBuilder.array([this.createCustomerAddress(addressItem)])
+            
+        });
+    }
+
+    onShown(): void {
+        document.getElementById('name').focus();
+    }
+
+    close(): void {
+        this.active = false;
+        this.modal.hide();
+    }
+
+    save() : void {
+
     }
 
 
